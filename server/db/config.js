35689891191bf622460e199749001ce7dff0f203
Matt Dubie <mbdubie@gmail.com>
2016-07-21 16:22:32 -0700

    added variable to manage the deployment mongo uri

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,14 @@
+var isDepoyed = true;
+var mongoUri = isDepoyed
+  ? 'mongodb://mdubie:ttaM3199@ds027165.mlab.com:27165/crumbs'
+  : 'mongodb://127.0.0.1:27017/crumbs2';
+
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 // Create a new key on the module.exports so server.js can access the connection property from db.
 module.exports.connect = function() {
-  mongoose.connect('mongodb://mdubie:ttaM3199@ds027165.mlab.com:27165/crumbs', function(err) {
+  mongoose.connect(mongoUri, function(err) {
     err ? console.log(err) : console.log('db connected');
   });
 };
