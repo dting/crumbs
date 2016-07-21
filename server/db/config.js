56cc015e8354e1bd84552680343cd63316cbfb3c
Matt Dubie <mbdubie@gmail.com>
2016-07-21 16:12:32 -0700

    added mongo uri

--- server/db/config.js
+++ server/db/config.js
@@ -3,7 +3,7 @@ mongoose.Promise = require('bluebird');
 
 // Create a new key on the module.exports so server.js can access the connection property from db.
 module.exports.connect = function() {
-  mongoose.connect('mongodb://127.0.0.1:27017/crumbs2', function(err) {
+  mongoose.connect('mongodb://mdubie:ttaM3199@ds027165.mlab.com:27165/crumbs', function(err) {
     err ? console.log(err) : console.log('db connected');
   });
 };
