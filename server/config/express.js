96864bb169f6e80a505c743396f6556db3ba491e
Dennis Ting <dennisting@gmail.com>
2016-09-04 19:08:53 -0700

    Enable CORS

--- server/config/express.js
+++ server/config/express.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
 
 module.exports = app => {
+  app.use(cors());
   app.use(express.static(path.join(__dirname, '../../public')));
   app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
