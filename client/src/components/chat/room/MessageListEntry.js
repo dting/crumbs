641dc857e54a4fd89266b490b760f15c4cd3b0ca
Dennis Ting <dennisting@gmail.com>
2016-09-06 23:31:15 -0700

    Style chat room to be closer to mobile client

--- client/src/components/chat/room/MessageListEntry.js
+++ client/src/components/chat/room/MessageListEntry.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Moment from 'moment';
 import { ListGroupItem } from 'react-bootstrap';
+import s from './room.css';
 
 export const MessageListEntry = props => {
   const { username, message, createdAt } = props.message;
   return (
     <ListGroupItem>
-      {`${username} ${message} ${Moment(createdAt).fromNow()}`}
+      <div className={s.messageContainer}>
+        <img src={require('../../../assets/profileIcon.png')}
+             alt="profile"
+             className={s.avatar}
+        />
+        <div className={s.messageContents}>
+          <div className={s.titleLabel}>{message}</div>
+          <div className={s.memberLabel}>{username}</div>
+          <div className={s.memberLabel}>{Moment(createdAt).fromNow()}</div>
+        </div>
+      </div>
     </ListGroupItem>
   );
 };
